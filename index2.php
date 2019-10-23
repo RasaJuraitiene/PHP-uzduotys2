9f4bfed56ea8b4822e953420844e3704d53da8f7
Rasa_Jur <vozgirdaite.rasa@gmail.com>
2019-10-23 16:48:19 +0300

    drinks card

--- index2.php
+++ index2.php
@@ -1,80 +1,60 @@
 <?php
 $drinks = [
     [
-        "name" => "Cenote™  Blanco",
-        "price_stock" => 64,
-        "discount" => 10,
-        "img" => "http://www.bennet.lt/cache/images/e4a6856510efef3ffec97f63b196f8bf.JPG",
+        'name' => 'Cenote™  Blanco',
+        'price_stock' => 64,
+        'discount' => 10,
+        'img' => 'http://www.bennet.lt/cache/images/e4a6856510efef3ffec97f63b196f8bf.JPG',
     ],
     [
-        "name" => "RUSSKIJ REZERV PREMIUM",
-        "price_stock" => 9.49,
-        "discount" => 20,
-        "img" => "http://www.bennet.lt/cache/images/de1fb9e4ced07f6d6afd7ceb38881efc.png",
+        'name' => 'RUSSKIJ REZERV PREMIUM',
+        'price_stock' => 9.49,
+        'discount' => 20,
+        'img' => "http://www.bennet.lt/cache/images/de1fb9e4ced07f6d6afd7ceb38881efc.png",
     ],
     [
-        "name" => "GASPADORIŲ TAMSUSIS 8",
-        "price_stock" => 1.45,
-        "discount" => 0,
-        "img" => "http://www.bennet.lt/cache/images/6a7e71f7c7d57f53ff8ceea25efd9c30.jpg",
+        'name' => 'GASPADORIŲ TAMSUSIS 8',
+        'price_stock' => 1.45,
+        'discount' => 0,
+        'img' => 'http://www.bennet.lt/cache/images/6a7e71f7c7d57f53ff8ceea25efd9c30.jpg',
     ],
     [
-        "name" => "BULMERS ORIGINAL",
-        "price_stock" => 3.09,
-        "discount" => 5,
-        "img" => "http://www.bennet.lt/cache/images/498bc9661b9eeb6d208b4291e5468930.JPG",
+        'name' => 'BULMERS ORIGINAL',
+        'price_stock' => 3.09,
+        'discount' => 5,
+        'img' => 'http://www.bennet.lt/cache/images/498bc9661b9eeb6d208b4291e5468930.JPG',
     ],
 ];
 
 foreach ($drinks as $key => $value){
-    $drinks[$key]["price_retail"] = $drinks[$key]["price_stock"]- $drinks[$key]["price_stock"] * $drinks[$key]["discount"]/100;
+    $drinks[$key]['price_retail'] = $value['price_stock']- $value['price_stock'] * $value['discount']/100;
 }
-
-var_dump ($drinks);
-
-//$rasinys = '';
-////$count_index = count($zodziai);
-////$random_index = rand(0, $count_index -1);
-//
-//for ($i = 0; $i < rand(100, 300); $i++) {
-//    $random_word = $zodziai[(rand(0, count($zodziai) - 1))];
-//    $rasinys .= " $random_word";
-//}
-//
-
-//foreach ($my_memories as $my_memory) {
-//    if (in_array($my_memories, $friend_memories)) {
-//        if(!in_array($my_memory, $common_memories))
-//        $common_memories[] = $my_memory;
-//    }
-//}
-//
-//
-//$h1 = 'Lietuvių egzaminas';
-//$p = "$rasinys ";
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-grid.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-reboot.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-<!---->
-<!--<h1>--><?php //print $h1; ?><!--</h1>-->
-<!--<p>--><?php //print $p; ?><!--</p>-->
-<!--        <ul>-->
-<!--            --><?php //foreach ($my_memories as $value): ?>
-<!--                <li>--><?php //print $value; ?><!--</li>-->
-<!--            --><?php //endforeach; ?>
-<!--        </ul>-->
-<!--       <h3>--><?php ////print $h3; ?><!--</h3>-->
-<!---->
-<!--        <h3>--><?php //print $h3; ?><!--</h3>-->
-<!--        <ul>-->
-<!--            --><?php //foreach ($friend_memories as $value): ?>
-<!--                <li>--><?php //print $value; ?><!--</li>-->
-<!--            --><?php //endforeach; ?>
-<!--        </ul>-->
+
+<div>
+
+    <?php foreach ($drinks as $key => $value): ?>
+        <div class=" card border col-4 text-center d-flex flex-center m-2">
+            <img src="<?php print $value['img']; ?>" alt="">
+            <h2><?php print $value['name']; ?></h2>
+            <p><?php print $value['price_retail'] ; ?></p>
+        </div>
+    <?php endforeach; ?>
+    <!-- bootstrap js  -->
+    <script src="assets/js/bootstrap.js"></script>
+    <script src="assets/js/bootstrap.bundle.js"></script>
+    <script src="assets/js/jqery.js"></script>
 </body>
 </html>
