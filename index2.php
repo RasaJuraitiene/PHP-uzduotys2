0b02e436f83a0c3625e1cf640ce636eab6bc6d34
Rasa_Jur <vozgirdaite.rasa@gmail.com>
2019-10-23 15:53:24 +0300

    drinks

--- index2.php
+++ index2.php
@@ -1,35 +1,42 @@
 <?php
-$zodziai = [
-    'Kaip',
-    'kartais',
-    'užknisa',
-    'kai',
-    'net',
-    'nesupranti',
-    'užduoties',
-    'o',
-    'reikia',
-    'ja',
-    'atlikti',
-    'ir',
-    'padaryti',
-    'ja',
-    'teisingai',
-    'ir',
-    'greitai',
-    'greiciau',
-    'uz',
-    'kitus'
+$drinks = [
+    [
+        "name" => "Cenote™  Blanco",
+        "price_stock" => 64,
+        "discount" => 0,
+        "img" => "http://www.bennet.lt/cache/images/e4a6856510efef3ffec97f63b196f8bf.JPG",
+    ],
+    [
+        "name" => "RUSSKIJ REZERV PREMIUM",
+        "price_stock" => 9.49,
+        "discount" => 20,
+        "img" => "http://www.bennet.lt/cache/images/de1fb9e4ced07f6d6afd7ceb38881efc.png",
+    ],
+    [
+        "name" => "GASPADORIŲ TAMSUSIS 8",
+        "price_stock" => 1.45,
+        "discount" => 7,
+        "img" => "http://www.bennet.lt/cache/images/6a7e71f7c7d57f53ff8ceea25efd9c30.jpg",
+    ],
+    [
+        "name" => "BULMERS ORIGINAL",
+        "price_stock" => 3.09,
+        "discount" => 5,
+        "img" => "http://www.bennet.lt/cache/images/498bc9661b9eeb6d208b4291e5468930.JPG",
+    ],
 ];
-$rasinys = '';
-//$count_index = count($zodziai);
-//$random_index = rand(0, $count_index -1);
 
-for($i = 0; $i < rand(100, 300); $i++) {
-    $random_word = $zodziai[(rand(0, count($zodziai) -1))];
-    $rasinys .= " $random_word";
-}
+var_dump ($drinks);
 
+//$rasinys = '';
+////$count_index = count($zodziai);
+////$random_index = rand(0, $count_index -1);
+//
+//for ($i = 0; $i < rand(100, 300); $i++) {
+//    $random_word = $zodziai[(rand(0, count($zodziai) - 1))];
+//    $rasinys .= " $random_word";
+//}
+//
 
 //foreach ($my_memories as $my_memory) {
 //    if (in_array($my_memories, $friend_memories)) {
@@ -37,21 +44,21 @@ for($i = 0; $i < rand(100, 300); $i++) {
 //        $common_memories[] = $my_memory;
 //    }
 //}
-
-
-$h1 = 'Lietuvių egzaminas';
-$p = "$rasinys ";
+//
+//
+//$h1 = 'Lietuvių egzaminas';
+//$p = "$rasinys ";
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-    </head>
-    <body>
-
-        <h1><?php print $h1; ?></h1>
-        <p><?php print $p; ?></p>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+<!---->
+<!--<h1>--><?php //print $h1; ?><!--</h1>-->
+<!--<p>--><?php //print $p; ?><!--</p>-->
 <!--        <ul>-->
 <!--            --><?php //foreach ($my_memories as $value): ?>
 <!--                <li>--><?php //print $value; ?><!--</li>-->
@@ -65,6 +72,5 @@ $p = "$rasinys ";
 <!--                <li>--><?php //print $value; ?><!--</li>-->
 <!--            --><?php //endforeach; ?>
 <!--        </ul>-->
-
-    </body>
+</body>
 </html>
