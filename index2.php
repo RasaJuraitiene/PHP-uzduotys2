02ef174e8bb39780fe859f49fb927f4cd8c7ca9f
Rasa_Jur <vozgirdaite.rasa@gmail.com>
2019-10-24 14:51:49 +0300

    alkoholis su kainomis

--- index2.php
+++ index2.php
@@ -3,7 +3,7 @@ $drinks = [
     [
         'name' => 'Cenote™  Blanco',
         'price_stock' => 64,
-        'discount' => 10,
+        'discount' => 12,
         'img' => 'http://www.bennet.lt/cache/images/e4a6856510efef3ffec97f63b196f8bf.JPG',
     ],
     [
@@ -27,9 +27,10 @@ $drinks = [
 ];
 
 foreach ($drinks as $key => $value){
-    $drinks[$key]['price_retail'] = $value['price_stock']- $value['price_stock'] * $value['discount']/100;
+    $drinks[$key]['price_retail'] = number_format((float)($value['price_stock']- $value['price_stock'] * $value['discount']/100), 2);
+    $drinks[$key]['price_retail_display'] = "&euro;" . $drinks[$key]['price_retail'];
+    $drinks[$key]['price_stock_display'] = "&euro;" . number_format($drinks[$key]['price_stock'], 2);
 }
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -40,16 +41,22 @@ foreach ($drinks as $key => $value){
     <link rel="stylesheet" href="assets/css/bootstrap-grid.css">
     <link rel="stylesheet" href="assets/css/bootstrap-reboot.css">
     <link rel="stylesheet" href="assets/css/style.css">
-</head>
+    <style>
+        .price_position-right{
+            top: 0;
+            right: 0;
+        }
+    </style>
+ </head>
 <body>
 
-<div>
-
+<div class="d-flex row">
     <?php foreach ($drinks as $key => $value): ?>
-        <div class=" card border col-4 text-center d-flex flex-center m-2">
-            <img src="<?php print $value['img']; ?>" alt="">
+        <div class="col-sm-3 border text-center position-relative m-2">
+            <div class="card-title bg-secondary position-absolute p-2 m-2"><?php print $value['price_stock_display']; ?></div>
+            <div class="card-title bg-danger position-absolute price_position-right p-3 m-2"><?php print $value['price_retail_display']; ?></div>
+            <img class="card-img" src="<?php print $value['img']; ?>" alt="">
             <h2><?php print $value['name']; ?></h2>
-            <p><?php print $value['price_retail'] ; ?></p>
         </div>
     <?php endforeach; ?>
     <!-- bootstrap js  -->
