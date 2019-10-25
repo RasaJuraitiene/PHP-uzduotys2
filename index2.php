ad2393e8d00374be4d53d769948795460a93f6d4
Rasa_Jur <vozgirdaite.rasa@gmail.com>
2019-10-25 14:07:31 +0300

    drinks

--- index2.php
+++ index2.php
@@ -1,7 +1,7 @@
 <?php
 $drinks = [
     [
-        'name' => 'Cenote™  Blanco',
+        'name' => 'Cenoteâ„¢  Blanco',
         'price_stock' => 64,
         'discount' => 12,
         'img' => 'http://www.bennet.lt/cache/images/e4a6856510efef3ffec97f63b196f8bf.JPG',
@@ -13,7 +13,7 @@ $drinks = [
         'img' => "http://www.bennet.lt/cache/images/de1fb9e4ced07f6d6afd7ceb38881efc.png",
     ],
     [
-        'name' => 'GASPADORIŲ TAMSUSIS 8',
+        'name' => 'GASPADORIÅ² TAMSUSIS 8',
         'price_stock' => 1.45,
         'discount' => 0,
         'img' => 'http://www.bennet.lt/cache/images/6a7e71f7c7d57f53ff8ceea25efd9c30.jpg',
@@ -26,49 +26,106 @@ $drinks = [
     ],
 ];
 
-foreach ($drinks as $key => $value) {
-    $drinks[$key]['price_retail'] = number_format((float)($value['price_stock'] - $value['price_stock'] * $value['discount'] / 100), 2);
-    $drinks[$key]['price_retail_display'] = "&euro;" . $drinks[$key]['price_retail'];
-    $drinks[$key]['price_stock_display'] = "&euro;" . number_format($drinks[$key]['price_stock'], 2);
+foreach ($drinks as $key => $drink) {
+    $drinks[$key]['price_retail'] = $drink['price_stock'] * (1 - $drink['discount'] * 0.01);
+    $drinks[$key]['price_retail_display'] = "&euro;" . number_format($drinks[$key]['price_retail'], 2);
 
-    if (rand(0, 1) > 0){
-        $drinks[$key]['in_stock'] = 1;
-    }else{
-        $drinks[$key]['in_stock'] = 'out of stock';
+    if ($drink['discount']) {
+        $drinks[$key]['price_stock_display'] = "&euro;" . number_format($drinks[$key]['price_stock'], 2);
+    }
+
+    $drinks[$key]['in_stock'] = rand(0, 1);
+
+    if ($drinks[$key]['in_stock']) {
+        $drinks[$key]['in_stock_class'] = 'in-stock';
+        $drinks[$key]['in_stock_display'] = 'Yra sandelyje';
+    } else {
+        $drinks[$key]['in_stock_class'] = 'not-in-stock';
+        $drinks[$key]['in_stock_display'] = 'Nera sandelyje';
+
+        unset($drinks[$key]['price_retail_display']);
+        unset($drinks[$key]['price_stock_display']);
     }
 }
-var_dump($drinks)
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
-    <link rel="stylesheet" href="assets/css/bootstrap.css">
-    <link rel="stylesheet" href="assets/css/bootstrap-grid.css">
-    <link rel="stylesheet" href="assets/css/bootstrap-reboot.css">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Gerti gerti</title>
     <style>
-        .price_position-right{
+        .drinks-container {
+            max-width: 800px;
+            position: relative;
+            display: flex;
+        }
+
+        .drink {
+            position: relative;
+            display: inline-block;
+            margin: 10px;
+            border: 1px solid gray;
+            height: 250px;
+            width: 200px;
+            text-align: center;
+        }
+
+        .drink-image {
+            object-fit: cover;
+            height: 150px;
+            margin: 0 auto;
+            padding: 5px;
+        }
+
+        .drink-name {
+            font-size: 16px;
+        }
+
+        .price {
+            display: block;
+            position: absolute;
+            color: white;
+            padding: 4px;
+        }
+
+        .price.stock {
+            background: gray;
+            left: 0;
             top: 0;
+        }
+
+        .price.retail {
+            background: red;
             right: 0;
+            top: 0;
+            padding: 6px;
+        }
+        .in-stock-class{
+            color: red;
+
+        }
+        .not-in-stock{
+            filter: grayscale(100%);
         }
     </style>
- </head>
+</head>
 <body>
+<div class="drinks-container">
+    <?php foreach ($drinks as $key => $drink): ?>
+        <div class="drink <?php print $drink['in_stock_class']; ?>">
+            <?php if (isset($drink['price_stock_display'])): ?>
+                <div class="price stock"><?php print $drink['price_stock_display']; ?></div>
+            <?php endif; ?>
 
-<div class="d-flex row">
-    <?php foreach ($drinks as $key => $value): ?>
-        <div class="col-sm-3 border text-center position-relative m-2">
-            <div class="card-title bg-secondary position-absolute p-2 m-2"><?php print $value['price_stock_display']; ?></div>
-            <div class="card-title bg-danger position-absolute price_position-right p-3 m-2"><?php print $value['price_retail_display']; ?></div>
-            <img class="card-img" src="<?php print $value['img']; ?>" alt="">
-            <h2><?php print $value['name']; ?></h2>
+            <?php if (isset($drink['price_retail_display'])): ?>
+                <div class="price retail"><?php print $drink['price_retail_display']; ?></div>
+            <?php endif; ?>
+
+            <img class="drink-image" src="<?php print $drink['img']; ?>">
+            <div class="drink-name"><?php print $drink['name']; ?></div>
+            <div class="drink-stock"><?php print $drink['in_stock_display']; ?></div>
         </div>
     <?php endforeach; ?>
-    <!-- bootstrap js  -->
-    <script src="assets/js/bootstrap.js"></script>
-    <script src="assets/js/bootstrap.bundle.js"></script>
-    <script src="assets/js/jqery.js"></script>
+</div>
 </body>
 </html>
